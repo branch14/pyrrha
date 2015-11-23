a1baaace9b4579a0e9afae1920a094c4da9a7ab0
phil <phil@branch14.org>
2015-11-23 22:12:15 +0100

    publish result to exchange

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # pyrrha
 
-Pyrrha will listen to a queue named `pyrrha` on RabbitMQ and transform the
-messages received into XSLX files, yes -- XSLX files.
+Pyrrha will listen to a queue named `pyrrha` on RabbitMQ and transform
+the messages received into XSLX files, yes -- XSLX files. The result
+will be published to an exchange called `pyrrha`.
 
 Currently it assumes that the message is encoded in JSON. The JSON
 should be a list of rows, while the list of rows should be lists of
@@ -27,9 +28,44 @@ For now start it with...
 
     lein run
 
+## Making use of...
+
+* https://github.com/mjul/docjure
+* http://reference.clojurerabbitmq.info/
+
+
+
 ## License
 
 Copyright © 2015 Phil Hofmann
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
+
+### Trivia
+
+In Greek mythology, Pyrrha (/ˈpɪrə/; Greek: Πύρρα) was the daughter of
+Epimetheus and Pandora and wife of Deucalion.
+
+When Zeus decided to end the Bronze Age with the great deluge,
+Deucalion and his wife, Pyrrha, were the only survivors. Even though
+he was imprisoned, Prometheus who could see the future and had
+foreseen the coming of this flood told his son, Deucalion, to build an
+ark and, thus, they survived. During the flood, they landed on Mount
+Parnassus, the only place spared by the flood.
+
+Once the deluge was over and the couple were on land again, Deucalion
+consulted an oracle of Themis about how to repopulate the earth. He
+was told to throw the bones of his mother behind his
+shoulder. Deucalion and Pyrrha understood the "mother" to be Gaia, the
+mother of all living things, and the "bones" to be rocks. They threw
+the rocks behind their shoulders, which soon began to lose their
+hardness and change form. Their mass grew greater, and the beginnings
+of human form emerged. The parts that were soft and moist became skin,
+the veins of the rock became people's veins, and the hardest parts of
+the rocks became bones. The stones thrown by Pyrrha became women;
+those thrown by Deucalion became men.
+
+Deucalion and Pyrrha had three sons, Hellen, Amphictyon, Orestheus;
+and three daughters Protogeneia, Pandora II and Thyia. (Source:
+[Wikipedia](https://en.wikipedia.org/wiki/Pyrrha))
--- src/pyrrha/core.clj
+++ src/pyrrha/core.clj
@@ -3,40 +3,49 @@
   (:require [langohr.core      :as rmq]
             [langohr.channel   :as lch]
             [langohr.queue     :as lq]
+            [langohr.exchange  :as lx]
             [langohr.consumers :as lc]
             [langohr.basic     :as lb]
             [dk.ative.docjure.spreadsheet :as dj]
             [cheshire.core     :as cs]))
 
 (def ^{:const true}
+  xslx-mime "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+(def ^{:const true}
   default-exchange-name "")
 
-(defn write-excel [data]
-  (dj/save-workbook! "my.xlsx" (dj/create-workbook "plan" data)))
+(def ^{:const true}
+  exchange-name "pyrrha")
+
+(def ^{:const true}
+  qname "pyrrha")
+
+(defn compile-xslx
+  [data]
+  (dj/save-workbook! "my.xlsx" (dj/create-workbook "plan" data))
+  (let [content (slurp "my.xlsx")]
+    (clojure.java.io/delete-file "my.xlsx")
+    content))
 
 (defn message-handler
-  [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-  (println (format "[consumer] Received a message: %s (%s), delivery tag: %d, content type: %s, type: %s"
+  [ch {:keys [content-type type] :as meta} ^bytes payload]
+  (println (format "[consumer] Received a message: %s, content type: %s, type: %s"
                    (String. payload "UTF-8")
-                   (String. payload "UTF-8")
-                   delivery-tag
                    content-type
                    type))
-  (write-excel (cs/parse-string (String. payload "UTF-8"))))
-    ;;(lb/publish ch default-exchange-name qname "Hello!" {:content-type "text/plain" :type "greetings.hi"})
-
-(defn start-consumer
-  "Starts a consumer in a separate thread"
-  [ch queue-name]
-  (lc/subscribe ch queue-name message-handler {:auto-ack true}))
+  (lb/publish ch
+              exchange-name
+              "done"
+              (compile-xslx (cs/parse-string (String. payload "UTF-8")))
+              {:content-type xslx-mime}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (let [conn  (rmq/connect)
-        ch    (lch/open conn)
-        qname "pyrrha"]
+        ch    (lch/open conn)]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+    (lx/declare ch exchange-name "topic")
     (lq/declare ch qname {:exclusive false :auto-delete true})
     (lc/blocking-subscribe ch qname message-handler {:auto-ack true})
     (rmq/close ch)
